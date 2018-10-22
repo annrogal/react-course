3d357b4a91429c27518f3ef4ce5fca0acfa4df5b
Anna Rogal <annrogal1@gmail.com>
2018-10-22 19:37:41 +0200

    dynamic component change

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>I am React App!</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m React app!'))
+     return (
+       <div className="App">
+         <h1>I am React App!</h1>
+        <Person />
+        <Person />
+        <Person />
+       </div>
+     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m React app!'))
   }
 }
 
