62775cd34ba00233d80e6f4ab7d76f7f2a760fd7
Anna Rogal <annrogal1@gmail.com>
2018-10-23 19:19:26 +0200

    define state in Component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [ 
+      { name: 'Max', age: 22 },
+      { name: 'Adam', age: 16 },
+      { name: 'Ada', age: 40 }
+    ]
+  }
   render() {
      return (
        <div className="App">
          <h1>I am React App!</h1>
-        <Person name="Jhon" age="22"/>
-        <Person name="Kate" age="16">My hobby: photography!</Person>
-        <Person name="Adam" age="45"/>
+         <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>y hobby is swimming</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>      
        </div>
      );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m React app!'))
