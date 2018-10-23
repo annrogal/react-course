2395593d47cfa8dbffa7edbf6ba08a7b5686be38
Anna Rogal <annrogal1@gmail.com>
2018-10-23 19:26:08 +0200

    add event to button

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -10,11 +10,19 @@ class App extends Component {
       { name: 'Ada', age: 40 }
     ]
   }
+
+  //dla wywoływanych eventów dodajemy w nazwie słówko Handler (good practice)
+  //funckję wywołujemy bez nawiasów (), przekazujemy tylko referencję
+  switchNameHandler = () => {
+    console.log('click test')
+  }
+
   render() {
      return (
        <div className="App">
          <h1>I am React App!</h1>
-         <button>Switch Name</button>
+        
+         <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>y hobby is swimming</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>      
