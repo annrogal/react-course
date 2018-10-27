eb1114d700f2851ee16ec9a827d1ac6b628f0538
Anna Rogal <annrogal1@gmail.com>
2018-10-27 19:50:52 +0200

    pass method with argument with porps to another component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,10 +13,10 @@ class App extends Component {
 
   //dla wywoływanych eventów dodajemy w nazwie słówko Handler (good practice)
   //funckję wywołujemy bez nawiasów (), przekazujemy tylko referencję
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //DO NOT DO THIS: this.state.persons[0].name = 'John'
     this.setState({persons: [ 
-      { name: 'Maximilina', age: 22 },
+      { name: newName, age: 22 },
       { name: 'Adam', age: 16 },
       { name: 'Ada', age: 36 }
     ]})
@@ -27,10 +27,14 @@ class App extends Component {
        <div className="App">
          <h1>I am React App!</h1>
         
-         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>y hobby is swimming</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>      
+         <button onClick={ () => this.switchNameHandler('Maximilian!!') }>Switch Name</button>
+        <Person name={ this.state.persons[0].name } 
+        age={ this.state.persons[0].age }/>
+        <Person name={ this.state.persons[1].name }
+         age={ this.state.persons[1].age } 
+         click={ this.switchNameHandler.bind(this, 'Max') }>My hobby is swimming</Person>
+        <Person name={ this.state.persons[2].name }
+         age={ this.state.persons[2].age }/>      
        </div>
      );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m React app!'))
